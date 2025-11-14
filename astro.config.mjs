b4fe7a3492ce4f8a5b51aecd3ae979808df097ce
Ricardo Robles <rroblesgo@gmail.com>
2025-11-14 21:03:34 +0100

    Ajustes iniciales tsconfig.json y astro.comfig.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,25 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+      },
+    },
+  },
 });
